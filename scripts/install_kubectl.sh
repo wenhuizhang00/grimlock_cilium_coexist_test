4e606f1eb7de11463f1562a3e9997ea7efd0bb26
wenhuizhang <wenhuizhang@roblox.com>
2025-12-18 18:06:14 -0800

    Create install_kubectl.sh

---
+++ scripts/install_kubectl.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key \
+  | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 0644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] \
+https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" \
+| sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+kubectl version --client
